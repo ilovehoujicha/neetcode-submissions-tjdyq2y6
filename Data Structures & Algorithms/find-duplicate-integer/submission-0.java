c762ecaed19c4a9460be9c710ebd529ecbf36cde
Matthew Soh <168527185+ilovehoujicha@users.noreply.github.com>
2026-04-16 23:26:53 +0800

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] == nums[i + 1]) return nums[i];
+            i++;
+        }
+        return 0;
+    }
+}
